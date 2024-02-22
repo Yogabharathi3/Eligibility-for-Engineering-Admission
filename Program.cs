17fba1f2b05adc6aea581d4667bd8ab3650d477f
Yogabharathi3 <118899387+Yogabharathi3@users.noreply.github.com>
2024-02-22 14:25:01 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,33 @@
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("Hiiiiiii");
+           int mpc,mp,maths, physics, chemistry;
+        string name;
+        Console.WriteLine("Enter mark in maths:");
+        maths = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter mark in physics:");
+        physics = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter mark in chemistry:");
+        chemistry = int.Parse(Console.ReadLine());
+        mpc=maths+physics+chemistry;
+        mp=maths+physics;
+        if(maths>=65&&physics>=55&&chemistry>=50)
+        {
+            if(mpc>=180||mp>=140)
+            {
+                Console.WriteLine("eligible for admission.");
+            }
+            else
+            {
+                Console.WriteLine("not eligible for admission.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("not eligible for admission.");
         }
     }
-
+        }
+        
